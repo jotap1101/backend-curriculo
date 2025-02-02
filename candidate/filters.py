f826e0d9ad49b6c9e1f23aa707ec646ecb7d923b
jotap1101 <jotap1101.notebook@gmail.com>
2025-02-02 10:31:28 -0300

    Refactor CandidateFilter and EmployeeFilter classes to inherit from FilterSet; remove unused filter logic.

--- candidate/filters.py
+++ candidate/filters.py
@@ -1,11 +1,5 @@
-from django_filters import rest_framework
+from django_filters.rest_framework import FilterSet
 from .models import Candidate
 
-class CandidateFilter(rest_framework.FilterSet):
-    has_disability = rest_framework.BooleanFilter(field_name='has_disability')
-    has_drivers_license = rest_framework.BooleanFilter(field_name='has_drivers_license')
-    is_first_job = rest_framework.BooleanFilter(field_name='is_first_job')
-
-    class Meta:
-        model = Candidate
-        fields = ['has_disability', 'has_drivers_license', 'is_first_job']
+class CandidateFilter(FilterSet):
+    pass
--- employee/filters.py
+++ employee/filters.py
@@ -1,11 +1,5 @@
-from django_filters import rest_framework
+from django_filters.rest_framework import FilterSet
 from .models import Employee
 
-class EmployeeFilter(rest_framework.FilterSet):
-    is_active = rest_framework.BooleanFilter(field_name='is_active')
-    is_staff = rest_framework.BooleanFilter(field_name='is_staff')
-    is_superuser = rest_framework.BooleanFilter(field_name='is_superuser')
-
-    class Meta:
-        model = Employee
-        fields = ['is_active', 'is_staff', 'is_superuser']
+class EmployeeFilter(FilterSet):
+    pass
